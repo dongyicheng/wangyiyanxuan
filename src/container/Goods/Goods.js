d5711d1b1ffd3974fb50a418bedaff710ddd71a2
dongyicheng <964011515@qq.com>
2018-11-03 13:03:32 +0800

    new

--- src/container/Goods/Goods.js
+++ src/container/Goods/Goods.js
@@ -1,35 +1,11 @@
-import React, {Component} from 'react'
-import {NavLink} from 'react-router-dom'
-import './index.less'
-import GoodNav from './goodnav'
-import GoodSlider from './goodSlider'
-import GoodSliders from './goodSliders'
-import GoodList from './goodlist'
-import {connect} from 'react-redux'
-import actions from '../../store/action/goods'
-import Modal from '../../component/AlertGoods/alertgoods'
+import React, { Component } from 'react'
 
-class Goods extends Component {
-    constructor() {
-        super();
-        this.state={show:false}
-    }
-
-    componentDidMount() {
-        //发送请求
-        this.props.getslider();
-        this.props.getList();
-    }
-    switchModal = () => this.setState({show: !this.state.show})
-    render() {
-       let  show=this.state.show
-        return <div>
-            <GoodNav></GoodNav>
-            <GoodSlider sliders={this.props.sliders}></GoodSlider>
-            <GoodSliders list={this.props.lessons.list}></GoodSliders>
-            <GoodList></GoodList>
-        </div>
-    }
+export default class Goods extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default connect(state => ({...state.goods}), actions)(Goods)
